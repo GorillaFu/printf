c99d1d7e7ba028e9590ef792cb86ea3a274aa948
angel19951 <2384@holbertonschool.com>
2020-10-30 14:34:43 -0400

    Printf function implementation | C | git rekt

--- _printf.c
+++ _printf.c
@@ -1,59 +1,16 @@
 #include "holberton.h"
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
 
-
-/**
- * _printf - prints arguments
- * @format: arguments to be printed
- * Return: number of chars
- */
-int _printf(const char *format, ...)
+int _printf(const char *format,...)
 {
 	va_list arg;
-	char *str;
-	const char *reader;
-	int index, count;
+	int count;
 
 	va_start(arg, format);
-	reader = format;
-	count = 0;
-	while (*reader != '\0')
-	{
-		if (*reader != '%')
-		{
-			putchar(*reader);
-		}
-		else
-		{
-			reader++;
-			if (*reader != '\0')
-			{
-				switch (*reader)
-				{
-				case 's':
-					str = va_arg(arg, char *);
-					while (*str != '\0')
-					{
-						putchar(*str);
-						str++;
-						count++;
-					}
-					count--;
-					break;
-				case 'c':
-					index = va_arg(arg, int);
-					putchar(index);
-					break;
-				case '%':
-					putchar('%');
-					break;
-				}
-			}
-		}
-		count++;
-	reader++;
-	}
+
+	count = vfprintf(stdout, format, arg);
+
 	va_end(arg);
-	return (count - 1);
+	return (count);
 }
