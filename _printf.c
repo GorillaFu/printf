37b137bbd5d976056a7a841795c7962a029630f5
angel19951 <2384@holbertonschool.com>
2020-11-06 15:47:48 -0400

    Printf fixing and debugging

--- _printf.c
+++ _printf.c
@@ -1,22 +1,58 @@
 #include "holberton.h"
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
+
 
 /**
- * _printf - print anything
- * @format: to be printed
- * Return: length of string printed
+ * _printf - prints arguments
+ * @format: arguments to be printed
+ * Return: number of chars
  */
-
 int _printf(const char *format, ...)
 {
 	va_list arg;
-	int count;
-
+	char *str;
+	const char *reader;
+	int index, count;
 	va_start(arg, format);
+	count = 0;
 
-	count = vfprintf(stdout, format, arg);
-
+	while (*format != '\0')
+	{
+		if (*reader != '%')
+		{
+			putchar(*reader);
+		}
+		else
+		{
+			reader++;
+			if (*reader != '\0')
+			{
+				switch (*reader)
+				{
+				case 's':
+					str = va_arg(arg, char *);
+					while (*str != '\0')
+					{
+						putchar(*str);
+						str++;
+						count++;
+					}
+					count--;
+					break;
+				case 'c':
+					index = va_arg(arg, int);
+					putchar(index);
+					break;
+				case '%':
+					putchar('%');
+					break;
+				}
+			}
+		}
+		count++;
+	reader++;
+	}
 	va_end(arg);
-	return (count);
+	return (0);
 }
